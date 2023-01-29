1fd502bc3967bf55c363ccc6f63fa526fdc19f13
Philip Demand <philipdemand@yahoo.com>
2023-01-28 19:11:27 -0500

    commit tree ce764be72e2629c1201d8ce476448fcd2e971e50

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+class Polygon {
+    constructor (polygon) {
+        this.polygon = polygon
+        //console.log(this.polygon)
+    }
+    get countSides() {
+        return this.polygon.length
+    }
+    get perimeter() {
+        return this.polygon.reduce((a,b)=>a+b)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if ((this.polygon[0] + this.polygon[1]) > this.polygon[2] && (this.polygon[1] + this.polygon[2]) > this.polygon[0] && (this.polygon[2] + this.polygon[0] > this.polygon[1])){
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.polygon.every( (val, i, arr) => val === arr[0] )
+    }
+    get area() {
+        return this.polygon[0] * this.polygon[1]
+    }
+}
